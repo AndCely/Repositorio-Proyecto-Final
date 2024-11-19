f94af356cd17bdf91c82c4530f32f449ccfdb0c7
Santiago Andres Cely Rincon <santiago.andresce@gmail.com>
2024-11-19 12:17:37 -0500

    Agregue carpeta de main

---
+++ Trabajo/settings.py
@@ -0,0 +1,12 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 700
+        self.bg_color = (35, 35, 35)
+        self.ship_speed = 1.5
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (211, 211, 211)
+        self.bullets_allowed = 3
