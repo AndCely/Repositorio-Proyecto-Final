ce13987f84a6e2b0dc198f74da9c3cb05add00ea
Santiago Andres Cely Rincon <santiago.andresce@gmail.com>
2024-11-19 13:49:54 -0500

    Modificaciones de alien

--- bullet.py
+++ bullet.py
@@ -8,11 +8,11 @@ class Bullet(Sprite):
         """Create a bullet object at the ship's current position."""
         super().__init__()
         self.screen = ai_game.screen
-        self.setting = ai_game.setting
-        self.color = self.setting.bullet_color
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
 
         #Create a bullet rect at (0,0) and then set correct position.
-        self.rect = pygame.Rect(0,0,self.setting.bullet_width, self.setting.bullet_height)
+        self.rect = pygame.Rect(0,0,self.settings.bullet_width, self.settings.bullet_height)
         self.rect.midtop = ai_game.ship.rect.midtop
 
         #Store the bullet's position as a decimal value.
@@ -21,7 +21,7 @@ class Bullet(Sprite):
     def update(self):
         """Move the bullet up the screen"""
         # Update the decimal position of the bullet
-        self.y -= self.setting.bullet_speed
+        self.y -= self.settings.bullet_speed
         #Update the rect position.
         self.rect.y = self.y
 
